ad1708eac3c87b92d4c0a5439c7b1f034576166c
MG Potgieter <potgieter.m@belgiumcampus.ac.za>
2024-12-01 21:47:27 +0200

    Day 1 part 1

--- AdventOfCodeApp/Day1/ListDistanceSolver.cs
+++ AdventOfCodeApp/Day1/ListDistanceSolver.cs
@@ -11,7 +11,18 @@ namespace AdventOfCodeApp.Day1
         public int Solve(string input)
         {
             var lists = Parser.ParseLists(input);
-            return 0;
+            foreach (var list in lists)
+            {
+                list.Sort();
+            }
+
+            int totalDistance = 0;
+            for (int i = 0; i < lists[0].Count; i++)
+            {
+                totalDistance += Math.Abs(lists[0][i] - lists[1][i]);
+            }
+
+            return totalDistance;
         }
     }
 }
