c44cf6431ff6893fabe27d9a03b5ba263079b2c3
MG Potgieter <potgieter.m@belgiumcampus.ac.za>
2024-12-01 16:31:47 +0200

    Parsing and structure

---
+++ AdventOfCodeApp/Day1/ListDistanceSolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeApp.Day1
+{
+    public class ListDistanceSolver
+    {
+        public int Solve(string input)
+        {
+            var lists = ParseLists(input);
+            return 0;
+        }
+
+        private List<List<int>> ParseLists(string input)
+        {
+            var lines = input.Split("\n");
+            var cols = lines[0].Split("   ").Count();
+            var listOfLists = new List<List<int>>();
+            for (int i = 0; i < cols; i++)
+            {
+                listOfLists.Add(new List<int>());
+            }
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                var colsInLine = line.Split("   ");
+                for (int i = 0; i < colsInLine.Length; i++)
+                {
+                    listOfLists[i].Add(int.Parse(colsInLine[i]));
+                }
+            }
+            return listOfLists;
+        }
+    }
+}
--- AdventOfCodeApp/Program.cs
+++ AdventOfCodeApp/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AdventOfCodeApp.Day1;
+
+Console.WriteLine("THIS IS CRITICAL, WE NEED TO FIND THE CHIEF HISTORIAN");
+Console.WriteLine("Without him, Christmas will be ruined!");
+Console.WriteLine("We need to find him before it's too late!");
+Console.WriteLine("==========================================================");
+Console.WriteLine("Here is the set of tools to aid in finding him!");
+Console.WriteLine("Please select the required tool:");
+Console.WriteLine("1. Historian's Office Search List Distance");
+
+var input = Console.ReadLine();
+Console.WriteLine("Please insert the path of the text file containing your input for this tool");
+var path = Console.ReadLine();
+var text = File.ReadAllText(path);
+
+switch (input)
+{
+    case "1":
+        var solver = new ListDistanceSolver();
+        var result = solver.Solve(text);
+        Console.WriteLine($"The result is: {result}");
+        break;
+    default:
+        Console.WriteLine("Invalid input");
+        break;
+}
