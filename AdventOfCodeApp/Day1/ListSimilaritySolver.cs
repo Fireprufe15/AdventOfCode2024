911f1ad9d831c3ae38df399eb9e35129353c0d73
MG Potgieter <potgieter.m@belgiumcampus.ac.za>
2024-12-01 21:55:28 +0200

    Day 1 part 2

---
+++ AdventOfCodeApp/Day1/ListSimilaritySolver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeApp.Day1
+{
+    public class ListSimilaritySolver
+    {
+        public int Solve(string input)
+        {
+            var lists = Parser.ParseLists(input);
+
+            var totalSimilarity = 0;
+            foreach (var item in lists[0])
+            {
+                totalSimilarity += item * lists[1].Where(x => x == item).Count();
+            }
+
+            return totalSimilarity;
+        }
+    }
+}
